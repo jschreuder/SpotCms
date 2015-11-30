637c1a600aa21e78bf93aabd1fc49d53ad4ca17d
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2015-11-30 01:22:22 +0100

    Spec'ed JsonApiResponse & JsonApiErrorResponse classes

---
+++ spec/Common/Http/JsonApiErrorResponseSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Spot\Api\Common\Http;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Spot\Api\Common\Http\JsonApiErrorResponse;
+
+/** @mixin  JsonApiErrorResponse */
+class JsonApiErrorResponseSpec extends ObjectBehavior
+{
+    /** @var  string */
+    private $message;
+
+    /** @var  int */
+    private $code;
+
+    public function let()
+    {
+        $this->message = 'Test message';
+        $this->code = 418;
+        $this->beConstructedWith($this->message, $this->code);
+    }
+
+    public function it_isInitializable()
+    {
+        $this->shouldHaveType(JsonApiErrorResponse::class);
+    }
+
+    public function it_getsJsonApiContentType()
+    {
+        $this->getHeaderLine('Content-Type')->shouldReturn('application/vnd.api+json');
+    }
+
+    public function it_getJsonApiBody()
+    {
+        $body = $this->getBody();
+        $body->rewind();
+        $body->getContents()->shouldReturn('{"errors":[{"title":"Test message","status":"418"}]}');
+    }
+}
