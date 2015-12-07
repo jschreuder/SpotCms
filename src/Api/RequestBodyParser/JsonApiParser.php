d614ea22138d96b911a4e643f6283dc7119a58a3
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2015-12-08 00:12:38 +0100

    Added JsonApiParserSpec.

--- src/Api/RequestBodyParser/JsonApiParser.php
+++ src/Api/RequestBodyParser/JsonApiParser.php
@@ -34,7 +34,7 @@ class JsonApiParser implements ApplicationInterface
 
         // If an error occurred, duck out now
         if (json_last_error() !== JSON_ERROR_NONE) {
-            return new JsonApiErrorResponse(['error' => 'Invalid JSON, couldn\'t decode.'], 400);
+            return new JsonApiErrorResponse('Invalid JSON, couldn\'t decode.', 400);
         }
 
         // Everything is well, continue on with parsed JSON body
