40f63b0052f53885a1c020d70d2e1c755ec50289
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-05-01 01:11:35 +0200

    Added spec for DeleteFileHandler.

---
+++ spec/FileManager/Handler/DeleteFileHandlerSpec.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace spec\Spot\FileManager\Handler;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Spot\Api\Request\RequestInterface;
+use Spot\Api\Response\Message\NotFoundResponse;
+use Spot\Api\Response\ResponseException;
+use Spot\Api\Response\ResponseInterface;
+use Spot\Application\Request\ValidationFailedException;
+use Spot\DataModel\Repository\NoUniqueResultException;
+use Spot\FileManager\Entity\File;
+use Spot\FileManager\Handler\DeleteFileHandler;
+use Spot\FileManager\Repository\FileRepository;
+
+/** @mixin  DeleteFileHandler */
+class DeleteFileHandlerSpec extends ObjectBehavior
+{
+    /** @var  FileRepository */
+    private $fileRepository;
+
+    /** @var  LoggerInterface */
+    private $logger;
+
+    public function let(FileRepository $fileRepository, LoggerInterface $logger)
+    {
+        $this->fileRepository = $fileRepository;
+        $this->logger = $logger;
+        $this->beConstructedWith($fileRepository, $logger);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(DeleteFileHandler::class);
+    }
+
+    public function it_can_parse_a_HttpRequest(ServerRequestInterface $httpRequest)
+    {
+        $path = '/path/to/a/file.ext';
+        $attributes = ['path' => $path];
+
+        $request = $this->parseHttpRequest($httpRequest, $attributes);
+        $request->shouldHaveType(RequestInterface::class);
+        $request->getRequestName()->shouldReturn(DeleteFileHandler::MESSAGE);
+        $request['path']->shouldBe($attributes['path']);
+    }
+
+    public function it_errors_on_invalid_uuid_when_parsing_request(ServerRequestInterface $httpRequest)
+    {
+        $attributes = ['path' => '/'];
+        $this->shouldThrow(ValidationFailedException::class)->duringParseHttpRequest($httpRequest, $attributes);
+    }
+
+    public function it_can_execute_a_request(RequestInterface $request, File $file)
+    {
+        $path = '/path/to/a/file.ext';
+        $request->offsetGet('path')->willReturn($path);
+        $request->getAcceptContentType()->willReturn('*/*');
+        $this->fileRepository->getByFullPath($path)->willReturn($file);
+        $this->fileRepository->delete($file)->shouldBeCalled();
+
+        $response = $this->executeRequest($request);
+        $response->shouldHaveType(ResponseInterface::class);
+        $response->getResponseName()->shouldReturn(DeleteFileHandler::MESSAGE);
+        $response['data']->shouldBe($file);
+    }
+
+    public function it_can_execute_a_not_found_request(RequestInterface $request)
+    {
+        $path = '/path/to/a/file.ext';
+        $request->offsetGet('path')->willReturn($path);
+        $request->getAcceptContentType()->willReturn('*/*');
+
+        $this->fileRepository->getByFullPath($path)->willThrow(new NoUniqueResultException());
+
+        $response = $this->executeRequest($request);
+        $response->shouldHaveType(NotFoundResponse::class);
+    }
+
+    public function it_can_handle_exception_during_request(RequestInterface $request)
+    {
+        $path = '/path/to/a/file.ext';
+        $request->offsetGet('path')->willReturn($path);
+        $request->getAcceptContentType()->willReturn('*/*');
+
+        $this->fileRepository->getByFullPath($path)->willThrow(new \RuntimeException());
+
+        $this->shouldThrow(ResponseException::class)->duringExecuteRequest($request);
+    }
+}
