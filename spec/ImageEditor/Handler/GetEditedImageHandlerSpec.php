f5c9d01b58e1a17cb6050102d45fe3aeb0daf82f
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-05-16 12:08:57 +0200

    Added empty specs for GetEdited & StoreEdited handlers.

---
+++ spec/ImageEditor/Handler/GetEditedImageHandlerSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Spot\ImageEditor\Handler;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
+use Spot\FileManager\FileManagerHelper;
+use Spot\ImageEditor\Handler\GetEditedImageHandler;
+use Spot\ImageEditor\ImageEditor;
+use Spot\ImageEditor\Repository\ImageRepository;
+
+/** @mixin  GetEditedImageHandler */
+class GetEditedImageHandlerSpec extends ObjectBehavior
+{
+    /** @var  ImageRepository */
+    private $imageRepository;
+
+    /** @var  ImageEditor */
+    private $imageEditor;
+
+    /** @var  FileManagerHelper */
+    private $helper;
+
+    /** @var  LoggerInterface */
+    private $logger;
+
+    public function let(
+        ImageRepository $imageRepository,
+        ImageEditor $imageEditor,
+        FileManagerHelper $helper,
+        LoggerInterface $logger
+    )
+    {
+        $this->imageRepository = $imageRepository;
+        $this->imageEditor = $imageEditor;
+        $this->helper = $helper;
+        $this->logger = $logger;
+        $this->beConstructedWith($imageRepository, $imageEditor, $helper, $logger);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GetEditedImageHandler::class);
+    }
+}
