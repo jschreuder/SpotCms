c20e938d1544a178969fc71fb648aef4e332845c
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-07-28 00:45:16 +0200

    Added CLI and create-user command.

---
+++ app
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php declare(strict_types = 1);
+
+require __DIR__ . '/app_init.php';
+/** @var  \Pimple\Container $container */
+
+$application = new \Symfony\Component\Console\Application('SpotCms-CLI');
+
+$application->add(new \Spot\Auth\Command\CreateUserCommand($container['service.authentication']));
+
+$application->run();
---
+++ src/Auth/Command/CreateUserCommand.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types = 1);
+
+namespace Spot\Auth\Command;
+
+use Ramsey\Uuid\Uuid;
+use Spot\Auth\Entity\User;
+use Spot\Auth\Repository\UserRepository;
+use Spot\Auth\Service\AuthenticationService;
+use Spot\Auth\Value\EmailAddress;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    /** @var  AuthenticationService */
+    private $authenticationService;
+
+    public function __construct(AuthenticationService $authenticationService)
+    {
+        parent::__construct();
+        $this->authenticationService = $authenticationService;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('auth:create-user')
+            ->setDescription('Creates a new user')
+            ->addArgument('email-address', InputArgument::REQUIRED, 'User e-mail address is their username')
+            ->addArgument('password', InputArgument::REQUIRED, 'Password')
+            ->addArgument('display-name', InputArgument::OPTIONAL, 'The name with which the user is displayed');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->authenticationService->createUser(
+            EmailAddress::get($input->getArgument('email-address')),
+            $input->getArgument('password'),
+            $this->getDisplayName($input->getArgument('display-name'), $input->getArgument('email-address'))
+        );
+    }
+
+    private function getDisplayName(string $displayName, string $emailAddress) : string
+    {
+        if ($displayName) {
+            return $displayName;
+        }
+
+        $parts = explode('@', $emailAddress);
+        return $parts[0];
+    }
+}
