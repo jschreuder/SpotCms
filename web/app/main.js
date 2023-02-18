0f8179dc32434cc81e761bc0a6731a51953867c4
Jelmer Schreuder <github@jelmerschreuder.nl>
2023-02-18 11:31:31 +0100

    Removing old angular material

--- web/app/main.js
+++
@@ -1,7 +0,0 @@
-angular.module("SpotCms", ["ngMaterial"]);
-
-angular.module("SpotCms").controller("pageHeader", ["$mdSidenav", function ($mdSidenav) {
-    this.sideNavToggle = function () {
-        $mdSidenav('main-menu').toggle();
-    };
-}]);
